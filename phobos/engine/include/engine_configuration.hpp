1bc864791e2111b758d136458edf4774c05e9de3
ismael <ismael.mednez.matamoros@gmail.com>
2026-05-09 21:53:38 +0200

    Added configuration

---
+++ phobos/engine/include/engine_configuration.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "window.hpp"
+
+using namespace Phobos;
+
+namespace Phobos::Engine {
+
+    enum class InitializeComponentEnum : int {
+        Window = 1,
+        Graphics = 2,
+        Sound = 4,
+        Io = 8,
+        All = 16
+    };
+    using InitializeComponentEnumType = int;
+
+    struct Configuration {
+        InitializeComponentEnumType initializeComponents{
+                static_cast<InitializeComponentEnumType>(InitializeComponentEnum::All)};
+        Window::WindowConfiguration windowConfiguration;
+    };
+    
+}
